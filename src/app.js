e80805f9add41bed96559136d7264b41402581cf
Manuel Martret <manumartret@gmail.com>
2022-06-23 22:37:46 +0200

    correcting code

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const who = ["El perro", "Mi abuela", "Su tortuga", "Mi pajaro"];
+const what = ["come", "molesta", "lo aplasta", "lo rompe"];
+const when = [
+  "antes de clase",
+  "justo a tiempo",
+  "cuando termine",
+  "durante el almuerzo",
+  "mientras oraba"
+];
+
+function generateWord(arr) {
+  let word;
+  let random = Math.floor(Math.random() * arr.length);
+  for (var i = 0; i <= random; i++) {
+    word = arr[i];
+  }
+  return word;
+}
+
+function onLoad() {
+  let excuse =
+    generateWord(who) + " " + generateWord(what) + " " + generateWord(when);
+  let text = document.querySelector("#excuse");
+  text.innerHTML = excuse;
+}
+
+document.querySelector("#generator").addEventListener("click", function() {
+  onLoad();
+});
